36ab3a3309b6e2f362fb33324104baafb710e7aa
luzalex23 <alexluzendreyravi@gmail.com>
2022-10-13 11:32:46 -0300

    Creation module WebAoi ande new controllers

---
+++ CpmPedidos.Api/Controllers/AppBaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Api.Controllers
+{
+
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider ServiceProvider;
+
+        public AppBaseController(IServiceProvider serviceProvider)
+        {
+
+        }
+    }
+}
---
+++ CpmPedidos.Api/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Api
+{
+    public class DependecyInjection
+    {
+        public static void Register(IServiceCollection serviceProvider)
+        {
+            RepositoryDependence(serviceProvider);
+        }
+
+        private static void RepositoryDependence(IServiceCollection serviceProvider)
+        {
+            
+        }
+    }
+}
