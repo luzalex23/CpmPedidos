0f6b4ef5a4624211f069e683a51736fdb825ddc6
luzalex23 <alexluzendreyravi@gmail.com>
2022-10-23 11:57:52 -0300

    Creat class DesignTimeDbContextFactory and Conections Configurations

---
+++ CpmPedidos.Repositorie/Common/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using CpmPedidos.Repositorie.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace CpmPedidos.Repository
+{
+    /*Classe que permite ultilizar a mesma string de conexão em outros projetos*/
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var fileName = Directory.GetCurrentDirectory() + $"/../CpmPedidos.API/appsettings.{environmentName}.json";
+
+            var configuration = new ConfigurationBuilder().AddJsonFile(fileName).Build();
+            var connectionString = configuration.GetConnectionString("App");
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseNpgsql(connectionString);
+
+            return new AppDbContext(builder.Options);
+        }
+    }
+}
